e6a785d5a411151c00c70becd120b62f1648968e
Ame <diphokoame@gmail.com>
2020-01-27 10:44:19 +0200

    update models

--- se_dmc/models/project.py
+++ se_dmc/models/project.py
@@ -5,5 +5,5 @@ class Project(models.Model):
     
     name = models.CharField(max_length=200, verbose_name='Name')
     description = models.TextField(verbose_name='Description')
-    duration = models.DecimalField(verbose_name='Project duration')
-    active = models.BooleanField(defaut=False, verbose_name='Is project ative')
+    duration = models.DecimalField(verbose_name='Project duration', decimal_places=1, max_digits=10)
+    active = models.BooleanField(default=False, verbose_name='Is project ative')
--- se_dmc/views/contact.py
+++ se_dmc/views/contact.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
-
+from ..models import Address
 
 class Contact(TemplateView):
     template_name = 'se_dmc/contact.html'
+    model = Address
+    context_object_name = 'contact_details'
