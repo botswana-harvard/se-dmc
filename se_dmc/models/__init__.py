3e0965a34433a0458b59ced908a8d828a2830d70
WackieMath <58945263+WackieMath@users.noreply.github.com>
2020-02-17 10:18:05 +0200

    Update __init__.py

--- se_dmc/models/__init__.py
+++ se_dmc/models/__init__.py
@@ -3,3 +3,7 @@ from .project import Project
 from .services import Service
 from .team import Team
 from .testimonials import Testimonial
+from .contactform import ContactForm
+from .forms import contactinfo
+
+
