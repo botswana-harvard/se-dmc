b70eaac0dc0d7da9d17f6aceaa405b03ba1926ca
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-02-25 07:47:36 +0200

    add description to document

--- se_dmc/models/document.py
+++ se_dmc/models/document.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Document(models.Model):
     title = models.CharField(max_length=100)
+    description = models.TextField()
     file = models.FileField(upload_to='documents/')
 
     def __str__(self):
