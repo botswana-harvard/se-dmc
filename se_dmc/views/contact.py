ab09a4b8d9e7ad4becb8c77944ff8e3ca23079fb
WackieMath <58945263+WackieMath@users.noreply.github.com>
2020-02-28 14:52:30 +0200

    Update contact.py

--- se_dmc/views/contact.py
+++ se_dmc/views/contact.py
@@ -1,42 +1,60 @@
-from django.contrib import messages
-from django.core.mail import send_mail
-from django.db.models.functions import text
-from django.http import HttpResponseRedirect, request
-from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
-from ..models import ContactForm
-from ..models import contactinfo
+from django.conf import settings
 
+from django.core.checks import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from ..forms import ContactForm1
 
 class Contact(TemplateView):
-
     template_name = 'se_dmc/contact.html'
-    form_class = contactinfo
-    fields = ContactForm()
-    initial = {'key': 'word'}
 
-    def get(self, request, *args, **kwargs):
-        form = contactinfo()
-        return render(request, self.template_name, {'form': form})
+    def index(request):
 
-       def post(self, request):
-        form = contactinfo(request.POST)
-        if form.is_valid():
-            first_name = request.POST.get('first_name')
-            last_name = request.POST.get('last_name')
-            email = request.POST.get('email')
-            subject = request.POST.get('subject')
-            message = request.POST.get('message')
-            self.fields.save()
-            messages.success(request, "Thank you, message sent")
-            send_mail(subject, 'Message from: {0}{1}\nOf email: {2}\nSubject: {4}\nMessage: {3}'.format(first_name.title(), last_name.title()
-                      , email
-                      , message, subject), from_email=first_name+last_name, recipient_list=['wmathaka@bhp.org.bw', 'ckgathi@bhp.org.bw'],
+        if request.method == 'POST':
+            message = request.POST['message']
+            send_mail('Contact Form',
+                      message,
+                      settings.EMAIL_HOST_USER,
+                      ['thatokgamaetsile@gmail.com'],
                       fail_silently=False)
+        return render(request, 'se_dmc/contact.html')
 
-            return HttpResponseRedirect('contact')
+def contact(request):
+    if request.method == 'POST':
+        contact_form = ContactForm1(request.POST)
+
+        if contact_form.is_valid():
+
+            firstname = contact_form.cleaned_data['firstname']
+            lastname = contact_form.cleaned_data['lastname']
+            email = contact_form.cleaned_data['email']
+            subject = contact_form.cleaned_data['subject']
+            message = contact_form.cleaned_data['message']
+
+            recipients = ['tkgamaetsile@bhp.org.bw']
+            if email:
+                recipients.append(email)
+
+            content = 'Message: '+message+' \nFirst-Name: '+firstname+'\nLast-Name: '+lastname
+
+            send_mail(subject, content, email, recipients)
+            contact_form.save()
+            print('Message sent successfully.')
+            return redirect('contact')
         else:
-            messages.error(request, "Message not sent, Some fields missing")
-            args = {'form': form, 'text': text}
-            return render(request, self.template_name, args)
+            print('Error sending your Message')
+
+    else:
+        contact_form = ContactForm1(request.POST)
+
+    context = {
+        "contact_form": contact_form,
+    }
+    template = 'se_dmc/contact.html'
+    return render(request, template, context)
+
+
+
 
