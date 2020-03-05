dce8d6a8af98af112ee3e2e7a39c280514bff9da
WackieMath <58945263+WackieMath@users.noreply.github.com>
2020-03-05 14:24:42 +0200

    Update __init__.py

--- se_dmc/views/__init__.py
+++ se_dmc/views/__init__.py
@@ -6,4 +6,11 @@ from .about import About
 from .contact import Contact,contact
 from .policy import Policies
 from .document import Documents, document_list
+from .Tshilo_dikotla import TshiloDikotla
+from .Cancer import Cancer
+from .microbiome import Microbiome
+from .LIS import LIS
+from .EIT import EIT
+from .BCPP import BCPP
+from .ambition import Ambition
 
